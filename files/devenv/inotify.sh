f09f4641fb84045630e652a5977cde01f8b36b15
Paul Ahern <ahernp@ahernp.com>
2019-06-02 14:46:42 +0100

    Remove Pylint.

--- files/devenv/inotify.sh
+++ files/devenv/inotify.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 flake8 --append-config=/root/setup.cfg /work/ahernp.com
-pylint --rcfile=/root/.pylintrc /work/ahernp.com
 
 while inotifywait --recursive -e modify /work; do
     flake8 --append-config=/root/setup.cfg /work
-    pylint --rcfile=/root/.pylintrc /work
 done
